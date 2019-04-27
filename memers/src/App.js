f3b6c994e14f84642138d868d321467910025dde
serpentz <abyicherinet@gmail.com>
2019-04-27 10:28:47 -0400

    memers

---
+++ memers/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.ytimg.com/vi/aGd5HKpIVUE/maxresdefault.jpg" className="logo" alt="logo" />
+        <p>
+           looks like u missed ur spanish lessons today i have ur family.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
